e83222df4f212940a252609471790cb5bd8630d8
suriyanas1234 <126218250+suriyanas1234@users.noreply.github.com>
2023-04-18 15:02:57 -0400

    google tag
    this uses the g tag which is a preset instead of manually installing the plugin, which requires frequent installation of updates, this is done through docusaurus back end server. this is mainly a logistics update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -59,6 +59,15 @@ const config = {
         },
       },
     ],
+    [
+      '@docusaurus/preset-classic',
+      {
+        gtag: {
+          trackingID: 'G-S6RRDF6PG8',
+          anonymizeIP: true,
+        },
+      },
+    ],
   ],
 
   plugins: [
