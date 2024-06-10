93f6413b4a03a22efe654bb80f7d088c614423e9
Maria Salonga <97309013+mariasalonga@users.noreply.github.com>
2024-06-10 11:33:42 -0400

    chore: Remove unused files and update dependencies

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,7 +16,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/tutorial">
+            to="/Tutorials">
             Go to Tutorial
           </Link>     
         </div>
